63426aac270822024ed3aadaaea217cafd7dde9d
Artem Kovalskiy <Allarttrent@gmail.com>
2018-08-18 17:29:49 +0300

    fix

--- src/Controller/FormsController.php
+++ src/Controller/FormsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\form\ContactType;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
